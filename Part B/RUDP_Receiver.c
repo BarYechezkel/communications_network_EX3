f7f431f3d57509bfb579829c0c8f99c496bab11b
Noam Cohen <noam12345002@gmail.com>
2024-05-19 23:04:30 +0300

    19/5

--- Part B/RUDP_Receiver.c
+++ Part B/RUDP_Receiver.c
@@ -4,11 +4,11 @@
 #include <unistd.h>     // For the close function
 #include <string.h>     // For the memset function
 #include <time.h>
-#include <sys/time.h>
 #include <netinet/tcp.h>
 #include <stdlib.h>
 #include "RUDP_API.h"
 
+#define CLOCKS_PER_SEC_B ((__clock_t)1000000)
 
 /*
  * @brief The maximum number of senders that the receiver can handle.
@@ -51,7 +51,8 @@ int main(int argc, char* argv[])
     printf("receiver start\n");
 
     ///////paramters
-    double time_taken = 0;
+    clock_t start, end;
+    double time_taken;
     int RECEIVER_PORT = atoi(argv[2]);
 
 
@@ -82,50 +83,45 @@ int main(int argc, char* argv[])
     size_t total_recv = 0; // the total bytes received so far
 
     int receiver_listen = 1;
-    struct timeval start, end;
     while (receiver_listen)
     {
-        gettimeofday(&start, NULL);
         int arr[33] = { 0 };
 
-        // printf("%d",start);
+        start = clock();       // start measuring the time
         // check if the exit message was received
         int bytes_received = rudp_recv(sock, BUFFER_SIZE, arr);
-         if (bytes_received == -1)
-        { // check for errors
-            perror("rudp_recv faild");
-            return FAIL;
-        }
-        gettimeofday(&end, NULL);
-
-
-        printf("byte recive:%d\n",bytes_received);
-        printf("****************************************\n");
+         end = clock();
         if (bytes_received == -2)//got FIN
         {
             receiver_listen = 0;
         }
-      
-      
-        printf("time taken %f\n",((end.tv_sec - start.tv_sec) * 1000.0) + ((end.tv_usec - start.tv_usec) / 1000.0));
-        
+
+
+        if (bytes_received == -1)
+        { // check for errors
+            perror("rudp_recv faild");
+            return -1;
+        }
+
 
        
-
-
-        if (bytes_received > 0) {
-             printf("got total recv: %d\n", bytes_received);
+        
+if (bytes_received != -2) {
+            printf("got total recv: %d\n", bytes_received);
             total_recv += bytes_received;
+
+
             // Calculate time taken and average bandwidth
-            time_taken = ((end.tv_sec - start.tv_sec) * 1000.0) + ((end.tv_usec - start.tv_usec) / 1000.0);               // in milliseconds
-            double average_bandwidth = (bytes_received / (1024.0 * 1024.0)) / (time_taken / 1000.0);
+            time_taken = ((double)(end - start)) * 1000 / CLOCKS_PER_SEC_B;                      // in milliseconds
+            double average_bandwidth = (total_recv / (1024.0 * 1024.0)) / (time_taken / 1000.0); // in MB/s
+            
             fprintf(stats_file, "Run #%d Data: Time= %f ms ; Speed= %f MB/S\n", run, time_taken, average_bandwidth);
 
             total_avg_time += time_taken;
             total_avg_speed += average_bandwidth;
             run++;
-        }
-    }/////////////////TODO
+        }/////////////////TODO
+    }
     fprintf(stats_file, " Average time: %f ms\n", total_avg_time / (run - 1));
     fprintf(stats_file, " Average speed: %f MB/s\n", total_avg_speed / (run - 1));
 
@@ -136,7 +132,7 @@ int main(int argc, char* argv[])
     fclose(stats_file);
     remove("stats");
     // Close the sender's socket and continue to the next iteration.
-    fprintf(stdout, "Sender ip%s port:%d disconnected\n", "127.0.0.1", 5060);
+    fprintf(stdout, "Sender ip: %s port: %d disconnected\n", "127.0.0.1", 5060);
     fprintf(stdout, "Receiver finished!\n");
 
     return 0;
